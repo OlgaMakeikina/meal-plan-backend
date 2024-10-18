6a2d3ae41cc9a03ec45505f3df792b8a9b5ea31d
OlgaMakeikina <146967263+OlgaMakeikina@users.noreply.github.com>
2024-10-18 11:35:48 -0300

    Add files via upload

--- MealController.js
+++ MealController.js
@@ -17,10 +17,10 @@ module.exports.saveMeals = async (req, res) => {
 
 // DELETE
 module.exports.deleteMeal = async (req, res) => {
-    const { _id } = req.body;
+    const _id = req.params.id;
     MealModel.findByIdAndDelete(_id)
-    .then(() => res.send("Meal deleted"))
-}
+    .then(() => res.send('Deleted a meal'))
+   }
 
 // EDIT
 module.exports.editMeal = async (req, res) => {
--- MealRoutes.js
+++ MealRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', getMeal);
 router.post('/saveMeals', saveMeals)
-router.delete('/deleteMeal', deleteMeal)
+router.delete('/deleteMeal/:id', deleteMeal);
 router.put('/editMeal', editMeal)
 
 module.exports = router;
